32f23c0745dbf752e596e097a83eec99e018dd46
flynn <flynn@local>
2026-08-27 22:50:29 +0800

    升级核价器: 10模块 + BOM引擎基准价导入 + 供应商报价CSV导入 + sw缓存v2

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-const CACHE_NAME = 'cost-estimator-v1';
+const CACHE_NAME = 'cost-estimator-v2';
 const urlsToCache = ['./index.html', './manifest.json'];
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
